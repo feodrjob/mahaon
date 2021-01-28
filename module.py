33f2311938453746240366e4a5e9894877505bf8
fedor <feodor070305@gmail.com>
2021-01-28 17:53:44 +0600

    domik

--- module.py
+++ module.py
@@ -1,11 +1,18 @@
 k=0
+a=int(input('diapazon'))
+b=int(input('diapazon2'))
+
 with open('Perepis.txt','r+') as f:
     for i in f:
-        print(i)
-        i=int(i[-5:])
-        print (i)
-        if i<1978:
-            k+=1
+        #print(i)
+        x=int(i[-5:])
+        #print (x)
+        l=i.split()
+        if(a<x<b):
+            k=k+1
+            print(l[1],l[2],l[3])
+
+
 print(k)
 
-print('haha')
+
